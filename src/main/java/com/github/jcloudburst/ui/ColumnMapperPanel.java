29c0111ecef684280c973d0e981e58ef20812d05
Brandon Borkholder <borkhobs@gmail.com>
2012-11-25 22:11:34 -0500

    ColumnMapperPanel is in a better state, but not working smoothly yet

--- src/main/java/com/github/jcloudburst/ui/ColumnMapperPanel.java
+++ src/main/java/com/github/jcloudburst/ui/ColumnMapperPanel.java
@@ -1,6 +1,8 @@
 package com.github.jcloudburst.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.IOException;
@@ -15,7 +17,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.Vector;
 
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -23,6 +24,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingWorker;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
@@ -30,141 +33,104 @@ import net.miginfocom.swing.MigLayout;
 
 import com.github.jcloudburst.ColumnMapType;
 import com.github.jcloudburst.ColumnsMapGuesser;
-import com.github.jcloudburst.ColumnsType;
-import com.github.jcloudburst.ConfigurationType;
 import com.github.jcloudburst.DelimitedFileReader;
 import com.github.jcloudburst.DelimitedSource;
-import com.github.jcloudburst.JDBCType;
+import com.github.jcloudburst.ui.DatabaseConnectionPanel.ConnectionState;
 import com.github.jcloudburst.ui.TableChooserPanel.TableRef;
 
 @SuppressWarnings("serial")
 public class ColumnMapperPanel extends ConfigStepPanel {
   private List<ColumnMapType> columnMaps;
 
+  private ConnectionState lastColumnsState;
+  private List<ColumnInfo> columns;
+
+  private FileSourceState lastFieldState;
+  private List<FieldInfo> fields;
+
   public ColumnMapperPanel() {
     super("Columns");
   }
 
-  private void populateColumns(List<ColumnInfo> columns, List<FieldInfo> fields, List<FieldInfo> defaultFields) {
+  @Override
+  protected void flushConfigurationToUI() throws IllegalStateException {
     removeAll();
-
-    columnMaps = new ArrayList<>(columns.size());
-    for (ColumnInfo column : columns) {
-      ColumnMapType map = new ColumnMapType();
-      map.setDbColumn(column.name);
-      columnMaps.add(map);
-    }
-
-    Vector<FieldInfo> fieldsVector = new Vector<>(fields);
-
-    JPanel listPanel = new JPanel(new MigLayout("", "[|grow,sg|grow,sg|grow,sg|grow,sg]"));
-    add(new JLabel("Skip"));
-    add(new JLabel("DB Column"));
-    add(new JLabel("File Field"));
-    add(new JLabel("Fixed Value"));
-    add(new JLabel("Date Format"), "wrap");
-
-    for (int index = 0; index < columns.size(); index++) {
-      addColumn(index, listPanel, columns.get(index), fieldsVector, defaultFields.get(index));
-    }
-
-    setLayout(new BorderLayout());
-    add(new JScrollPane(listPanel), BorderLayout.CENTER);
+    
+    ensureHasColumns();
   }
 
-  private void addColumn(final int index, JPanel container, ColumnInfo column, Vector<FieldInfo> fields, FieldInfo defaultField) {
-    final JCheckBox skipBox = new JCheckBox();
-    JLabel columnInfoLabel = new JLabel(column.name + " (" + column.typeName + ")");
-    final JComboBox<FieldInfo> fieldChooser = new JComboBox<>(fields);
-    final JTextField dateFormatField = new JTextField("yyyy-MM-dd HH:mm:ss");
-    final JTextField fixedValueField = new JTextField();
-
-    skipBox.addItemListener(new ItemListener() {
-      @Override
-      public void itemStateChanged(ItemEvent e) {
-        fieldChooser.setEnabled(!skipBox.isSelected());
-        dateFormatField.setEnabled(!skipBox.isSelected());
-        fixedValueField.setEnabled(!skipBox.isSelected());
-      }
-    });
-
-    if (column.isDate()) {
-      dateFormatField.getDocument().addDocumentListener(new DocumentListener() {
-        @Override
-        public void removeUpdate(DocumentEvent e) {
-          changedUpdate(e);
-        }
-
-        @Override
-        public void insertUpdate(DocumentEvent e) {
-          changedUpdate(e);
-        }
-
-        @Override
-        public void changedUpdate(DocumentEvent e) {
-          columnMaps.get(index).setFormat(dateFormatField.getText());
-        }
-      });
-    }
-
-    fieldChooser.addItemListener(new ItemListener() {
-      @Override
-      public void itemStateChanged(ItemEvent e) {
-        FieldInfo field = (FieldInfo) fieldChooser.getSelectedItem();
-        if (field == null) {
-          columnMaps.get(index).setFileColIndex(null);
-        } else {
-          columnMaps.get(index).setFileColIndex(field.index);
-        }
-      }
-    });
-
-    fixedValueField.getDocument().addDocumentListener(new DocumentListener() {
-      @Override
-      public void removeUpdate(DocumentEvent e) {
-        changedUpdate(e);
-      }
-
-      @Override
-      public void insertUpdate(DocumentEvent e) {
-        changedUpdate(e);
-      }
-
-      @Override
-      public void changedUpdate(DocumentEvent e) {
-        columnMaps.get(index).setFixedValue(fixedValueField.getText());
-      }
-    });
-
-    skipBox.setEnabled(column.nullable);
-    fieldChooser.setSelectedItem(defaultField);
-    dateFormatField.setEditable(column.isDate());
-
-    container.add(skipBox, "grow");
-    container.add(columnInfoLabel, "grow");
-    container.add(fieldChooser, "grow");
-    container.add(fixedValueField, "grow");
-    container.add(dateFormatField, "grow,wrap");
+  @Override
+  protected void flushUIToConfiguration() throws IllegalStateException {
+    config.getMapping().getColumn().clear();
+    config.getMapping().getColumn().addAll(columnMaps);
   }
 
-  private List<ColumnInfo> getColumns(JDBCType connectionInfo, String table) throws SQLException {
+  private void ensureHasColumns() {
+    if (lastColumnsState == null || !lastColumnsState.equals(new ConnectionState(config))) {
+      columns = null;
+      new SwingWorker<List<ColumnInfo>, Void>() {
+        @Override
+        protected List<ColumnInfo> doInBackground() throws Exception {
+          return getColumns();
+        }
+
+        @Override
+        protected void done() {
+          try {
+            columns = get();
+            ensureHasFields();
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }
+      }.execute();
+    } else {
+      ensureHasFields();
+    }
+  }
+
+  private List<ColumnInfo> getColumns() throws SQLException {
     try (Connection c = getConnection()) {
-      TableRef tableRef = new TableRef(table);
+      TableRef tableRef = new TableRef(config.getTable());
       DatabaseMetaData metadata = c.getMetaData();
       ResultSet set = metadata.getColumns(tableRef.catalog, tableRef.schema, tableRef.name, null);
-
+  
       List<ColumnInfo> columns = new ArrayList<>();
       while (set.next()) {
         columns.add(new ColumnInfo(set));
       }
-
+  
       set.close();
       Collections.sort(columns);
       return columns;
     }
   }
 
-  private List<FieldInfo> getFields(ConfigurationType config) throws IOException {
+  private void ensureHasFields() {
+    if (lastFieldState == null || !lastFieldState.equals(new FileSourceState(config))) {
+      fields = null;
+      new SwingWorker<List<FieldInfo>, Void>() {
+        @Override
+        protected List<FieldInfo> doInBackground() throws Exception {
+          return getFields();
+        }
+
+        @Override
+        protected void done() {
+          try {
+            fields = get();
+            setupUI();
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }
+      }.execute();
+    } else {
+      setupUI();
+    }
+  }
+
+  private List<FieldInfo> getFields() throws IOException {
     List<String> fieldNames = null;
     for (DelimitedSource source : config.getCsv()) {
       DelimitedFileReader header = new DelimitedFileReader(source);
@@ -176,7 +142,7 @@ public class ColumnMapperPanel extends ConfigStepPanel {
         header.close();
       }
     }
-
+  
     List<FieldInfo> fields = null;
     if (fieldNames != null) {
       fields = new ArrayList<>(fieldNames.size());
@@ -184,79 +150,215 @@ public class ColumnMapperPanel extends ConfigStepPanel {
         FieldInfo field = new FieldInfo();
         field.name = name;
         field.index = fields.size();
-
+  
         fields.add(field);
       }
     }
-
+  
     return fields;
   }
 
-  private List<FieldInfo> getInitialFields(List<ColumnInfo> columns, List<FieldInfo> fields, ColumnsType columnMap) {
+  private void setupUI() {
+    removeAll();
+    
+    initializeColumnMaps();
+
+    JPanel listPanel = new JPanel(new MigLayout("", "[|grow,sg|grow,sg|grow,sg|grow,sg]"));
+    listPanel.add(new JLabel("Skip"));
+    listPanel.add(new JLabel("DB Column"));
+    listPanel.add(new JLabel("File Field"));
+    listPanel.add(new JLabel("Fixed Value"));
+    listPanel.add(new JLabel("Date Format"), "wrap");
+
+    for (int index = 0; index < columnMaps.size(); index++) {
+      addColumn(index, listPanel);
+    }
+
+    setLayout(new BorderLayout());
+    add(new JScrollPane(listPanel), BorderLayout.CENTER);
+    revalidate();
+  }
+
+  private void addColumn(final int index, JPanel container) {
+    final ColumnInfo column = columns.get(index);
+    final ColumnMapType map = columnMaps.get(index);
+  
+    final JCheckBox skipBox = new JCheckBox();
+    JLabel columnInfoLabel = new JLabel(column.name + " (" + column.typeName + ")");
+    final JComboBox<FieldInfo> fieldChooser = new JComboBox<>(fields.toArray(new FieldInfo[fields.size()]));
+    final JTextField dateFormatField = new JTextField("yyyy-MM-dd HH:mm:ss");
+    final JTextField fixedValueField = new JTextField();
+  
+    skipBox.addItemListener(new ItemListener() {
+      @Override
+      public void itemStateChanged(ItemEvent e) {
+        fieldChooser.setEnabled(!skipBox.isSelected());
+        dateFormatField.setEnabled(!skipBox.isSelected());
+        fixedValueField.setEnabled(!skipBox.isSelected());
+      }
+    });
+  
+    if (column.isDate()) {
+      dateFormatField.getDocument().addDocumentListener(new DocumentListener() {
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+          changedUpdate(e);
+        }
+  
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+          changedUpdate(e);
+        }
+  
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+          map.setFormat(dateFormatField.getText());
+        }
+      });
+    }
+  
+    fieldChooser.addItemListener(new ItemListener() {
+      @Override
+      public void itemStateChanged(ItemEvent e) {
+        FieldInfo field = (FieldInfo) fieldChooser.getSelectedItem();
+        if (field == null) {
+          map.setFileColIndex(null);
+          map.setFileColName(null);
+        } else if (field.name == null) {
+          map.setFileColIndex(field.index);
+          fixedValueField.setText(null);
+        } else {
+          map.setFileColName(field.name);
+          fixedValueField.setText(null);
+        }
+      }
+    });
+  
+    fixedValueField.getDocument().addDocumentListener(new DocumentListener() {
+      @Override
+      public void removeUpdate(DocumentEvent e) {
+        changedUpdate(e);
+      }
+  
+      @Override
+      public void insertUpdate(DocumentEvent e) {
+        changedUpdate(e);
+      }
+  
+      @Override
+      public void changedUpdate(DocumentEvent e) {
+        String text = fixedValueField.getText();
+        if (text == null || text.isEmpty()) {
+          map.setFixedValue(null);
+        } else {
+          map.setFixedValue(text);
+          fieldChooser.setSelectedItem(null);
+        }
+      }
+    });
+  
+    skipBox.setEnabled(column.nullable);
+    fieldChooser.setSelectedItem(getExistingMap(column));
+    dateFormatField.setVisible(column.isDate());
+  
+    container.add(skipBox, "grow");
+    container.add(columnInfoLabel, "grow");
+    container.add(fieldChooser, "grow");
+    container.add(fixedValueField, "grow");
+  
+    if (column.isDate()) {
+      container.add(dateFormatField, "grow,wrap");
+    } else {
+      JLabel notDate = new JLabel("not a recognized date column");
+      notDate.setFont(notDate.getFont().deriveFont(Font.ITALIC));
+      notDate.setForeground(Color.gray);
+      notDate.setHorizontalAlignment(SwingConstants.CENTER);
+      container.add(notDate, "grow,wrap");
+    }
+  }
+
+  private void initializeColumnMaps() {
+    Map<String, ColumnMapType> alreadyMapped = new TreeMap<>();
+    for (ColumnMapType map : config.getMapping().getColumn()) {
+      alreadyMapped.put(map.getDbColumn(), map);
+    }
+    
+    List<FieldInfo> guessedFields = getGuessedFields(columns, fields);
+    
+    columnMaps = new ArrayList<>(columns.size());
+    for (int i = 0; i < columns.size(); i++) {
+      ColumnInfo column = columns.get(i);
+      ColumnMapType map = alreadyMapped.get(column.name);
+      if (map == null) {
+        map = new ColumnMapType();
+        map.setDbColumn(column.name);
+        
+        FieldInfo guessedField = guessedFields.get(i);
+        if (guessedField != null) {
+          if (guessedField.name == null) {
+            map.setFileColIndex(guessedField.index);
+          } else {
+            map.setFileColName(guessedField.name);
+          }
+        }
+      }
+      
+      columnMaps.add(map);
+    }
+  }
+
+  private List<FieldInfo> getGuessedFields(List<ColumnInfo> columns, List<FieldInfo> fields) {
     if (fields == null) {
       return Arrays.asList(new FieldInfo[columns.size()]);
     }
-
+  
     List<String> colNames = new ArrayList<>(columns.size());
     for (ColumnInfo column : columns) {
       colNames.add(column.name);
     }
-
+  
     List<String> fieldNames = new ArrayList<>(fields.size());
     for (FieldInfo field : fields) {
       fieldNames.add(field.name);
     }
-
+  
     ColumnsMapGuesser guesser = new ColumnsMapGuesser(colNames, fieldNames);
     Map<Integer, Integer> map = new TreeMap<>(guesser.guessMapping());
-
-    // add existing mappings
-    for (ColumnMapType entry : columnMap.getColumn()) {
-      String col = entry.getDbColumn();
-      int colIndex = colNames.indexOf(col);
-
-      Integer fieldIndex = entry.getFileColIndex();
-      if (fieldIndex == null) {
-        String field = entry.getFileColName();
-        if (field != null) {
-          fieldIndex = fieldNames.indexOf(field);
-        }
-      }
-
-      if (colIndex >= 0 && fieldIndex >= 0) {
-        map.put(colIndex, fieldIndex);
-      }
-    }
-
-    List<FieldInfo> initialFields = new ArrayList<>(columns.size());
+  
+    List<FieldInfo> guessedFields = new ArrayList<>(columns.size());
     for (int i = 0; i < columns.size(); i++) {
       if (map.containsKey(i)) {
-        initialFields.add(fields.get(map.get(i)));
+        guessedFields.add(fields.get(map.get(i)));
       } else {
-        initialFields.add(null);
+        guessedFields.add(null);
       }
     }
-
-    return initialFields;
+  
+    return guessedFields;
   }
 
-  @Override
-  protected void flushConfigurationToUI() throws SQLException, IOException, IllegalStateException {
-    if (config.getMapping() == null) {
-      config.setMapping(new ColumnsType());
+  private FieldInfo getExistingMap(ColumnInfo column) {
+    ColumnMapType map = null;
+    for (ColumnMapType m : columnMaps) {
+      if (m.getDbColumn().equals(column.name)) {
+        map = m;
+        break;
+      }
     }
-
-    List<ColumnInfo> columns = getColumns(config.getJdbc(), config.getTable());
-    List<FieldInfo> fields = getFields(config);
-    List<FieldInfo> defaultFields = getInitialFields(columns, fields, config.getMapping());
-
-    populateColumns(columns, fields, defaultFields);
-  }
-
-  @Override
-  protected void flushUIToConfiguration() throws SQLException, IOException, IllegalStateException {
-    config.getMapping().getColumn().clear();
-    config.getMapping().getColumn().addAll(columnMaps);
+    
+    if (map == null) {
+      return null;
+    } else if (map.getFileColIndex() != null ) {
+      return fields.get(map.getFileColIndex());
+    } else if (map.getFileColName() != null) {
+      for (FieldInfo field : fields) {
+        if (map.getFileColName().equals(field.name)) {
+          return field;
+        }
+      }
+    }
+    
+    return null;
   }
 
   private static class ColumnInfo implements Comparable<ColumnInfo> {
---
+++ src/main/java/com/github/jcloudburst/ui/FileSourceState.java
@@ -0,0 +1,12 @@
+package com.github.jcloudburst.ui;
+
+import com.github.jcloudburst.ConfigurationType;
+import com.github.jcloudburst.ui.ConfigStepPanel.ConfigPartialState;
+
+public class FileSourceState extends ConfigPartialState {
+
+  public FileSourceState(ConfigurationType config) {
+    // TODO Auto-generated constructor stub
+  }
+
+}
